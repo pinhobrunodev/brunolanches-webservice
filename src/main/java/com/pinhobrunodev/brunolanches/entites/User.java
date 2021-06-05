9c74b95b7c80258b5fe87b3cc4c3166ee9e1c6cf
bruno pinho <iaishowcs@gmail.com>
2021-06-05 16:54:05 -0300

    Implement User class

---
+++ src/main/java/com/pinhobrunodev/brunolanches/entites/User.java
@@ -0,0 +1,130 @@
+package com.pinhobrunodev.brunolanches.entites;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull(message = "Name cannot be null")
+	private String name;
+	@NotNull(message = "Phone cannot be null")
+	@Size(min = 11, max = 11, message = "Enter a valid number, example : 75999999999")
+	private String phone;
+	@NotNull(message = "Email cannot be null")
+	@Email(message = "Email should be valid")
+	private String email;
+	@NotNull(message = "CPF cannot be null")
+	@CPF(message = "Enter a valid CPF")
+	private String cpf;
+	@NotNull
+	@JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDate date;
+
+	public User() {
+
+	}
+
+	public User(Long id, String name, String phone, String email, String cpf, LocalDate date) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.cpf = cpf;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
