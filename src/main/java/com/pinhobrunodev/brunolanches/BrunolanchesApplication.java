063a1d78c1436ccad6f22f598ef6cbefa67f33dc
bruno pinho <iaishowcs@gmail.com>
2021-06-05 16:17:28 -0300

    Project created

---
+++ src/main/java/com/pinhobrunodev/brunolanches/BrunolanchesApplication.java
@@ -0,0 +1,13 @@
+package com.pinhobrunodev.brunolanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunolanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunolanchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinhobrunodev/brunolanches/BrunolanchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinhobrunodev.brunolanches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunolanchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
