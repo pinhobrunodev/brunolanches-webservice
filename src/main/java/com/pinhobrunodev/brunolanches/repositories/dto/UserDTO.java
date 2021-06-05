7b6a728993a0e9e2115145af8eedfa9b2ccee02b
bruno pinho <iaishowcs@gmail.com>
2021-06-05 17:06:30 -0300

    Implement DTO

---
+++ src/main/java/com/pinhobrunodev/brunolanches/repositories/dto/UserDTO.java
@@ -0,0 +1,109 @@
+package com.pinhobrunodev.brunolanches.repositories.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.pinhobrunodev.brunolanches.entites.User;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String phone;
+	private String email;
+	private LocalDate date;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(Long id, String name, String phone, String email, LocalDate date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.date = date;
+	}
+	
+	// data to be sent to the client-side
+	
+	public UserDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+		phone = entity.getPhone();
+		email = entity.getEmail();
+		date = entity.getDate();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/pinhobrunodev/brunolanches/repositories/dto/UserInsertDTO.java
@@ -0,0 +1,96 @@
+package com.pinhobrunodev.brunolanches.repositories.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.pinhobrunodev.brunolanches.entites.User;
+
+public class UserInsertDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String phone;
+	private String email;
+	private LocalDate date;
+	private String cpf;
+	
+	public UserInsertDTO() {
+		
+	}
+	
+	public UserInsertDTO(Long id, String name, String phone, String email, LocalDate date, String cpf) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+		this.date = date;
+		this.cpf = cpf;
+	}
+	
+	// data that will be entered into the client-side
+	
+	public UserInsertDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+		phone = entity.getPhone();
+		email = entity.getEmail();
+		date = entity.getDate();
+		cpf = entity.getCpf();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+	
+}
