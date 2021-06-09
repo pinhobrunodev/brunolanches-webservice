c1b3b5407f5b717177369c2896d07a04e20a7851
bruno pinho <iaishowcs@gmail.com>
2021-06-09 14:52:10 -0300

    Implementing DeliverymanController

---
+++ src/main/java/com/pinhobrunodev/brunolanches/controller/DeliverymanController.java
@@ -0,0 +1,49 @@
+package com.pinhobrunodev.brunolanches.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.pinhobrunodev.brunolanches.dto.DeliverymanDTO;
+import com.pinhobrunodev.brunolanches.services.DeliverymanService;
+
+@RestController
+@RequestMapping(value = "/deliverymans")
+public class DeliverymanController {
+
+	@Autowired
+	private DeliverymanService service;
+	
+	@PostMapping(consumes =  MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<DeliverymanDTO> insert(DeliverymanDTO dto){
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(service.insert(dto));
+	}
+	@PutMapping(consumes =  MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<DeliverymanDTO> update(DeliverymanDTO dto){
+		return ResponseEntity.ok().body(service.update(dto));
+	}
+	@DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> delete(Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<DeliverymanDTO>  findByEmail(String email){
+		return ResponseEntity.ok().body(service.findByEmail(email));
+	}
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<DeliverymanDTO>> findAllOrderByNameASC(){
+		return ResponseEntity.ok().body(service.findAllOrderByNameASC());
+	}
+}
