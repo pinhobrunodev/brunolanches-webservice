6a76d0b439b1ede7ebab24950a021f77c88c49db
bruno pinho <iaishowcs@gmail.com>
2021-06-09 14:57:45 -0300

    :hammer: fix @RequestBody package

--- src/main/java/com/pinhobrunodev/brunolanches/controller/DeliverymanController.java
+++ src/main/java/com/pinhobrunodev/brunolanches/controller/DeliverymanController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.pinhobrunodev.brunolanches.dto.DeliverymanDTO;
 import com.pinhobrunodev.brunolanches.services.DeliverymanService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/deliverymans")
