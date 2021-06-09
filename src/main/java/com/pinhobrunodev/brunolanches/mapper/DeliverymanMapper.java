cf373b8f4e0be5d93b0798a6e5a471882207bda1
bruno pinho <iaishowcs@gmail.com>
2021-06-09 14:26:47 -0300

    Implement DeliverymanMapper

---
+++ src/main/java/com/pinhobrunodev/brunolanches/mapper/DeliverymanMapper.java
@@ -0,0 +1,31 @@
+package com.pinhobrunodev.brunolanches.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.pinhobrunodev.brunolanches.dto.DeliverymanDTO;
+import com.pinhobrunodev.brunolanches.entites.Deliveryman;
+
+@Component
+public class DeliverymanMapper {
+
+	
+	public Deliveryman toEntity(DeliverymanDTO dto) {
+		Deliveryman entity = new Deliveryman();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setPhone(dto.getPhone());
+		entity.setEmail(dto.getEmail());
+		return entity;
+	}
+	
+	
+	public DeliverymanDTO toDeliverymanDTO(Deliveryman entity) {
+		DeliverymanDTO dto = new DeliverymanDTO();
+		dto.setId(entity.getId());
+		dto.setCpf(entity.getCpf());
+		dto.setEmail(entity.getEmail());
+		dto.setName(entity.getName());
+		dto.setPhone(entity.getPhone());
+		return dto;
+	}
+}
