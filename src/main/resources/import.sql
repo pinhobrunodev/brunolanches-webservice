e4f370d74d03b6d37cbe1223521223802fada118
bruno pinho <iaishowcs@gmail.com>
2021-06-12 22:04:25 -0300

    Implement Seed Category SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_category(name) values ('PASTA');
+INSERT INTO tb_category(name) values ('PIZZA');
+INSERT INTO tb_category(name) values ('HAMBURGUER');
+INSERT INTO tb_category(name) values ('DESSERT');
+INSERT INTO tb_category(name) values ('DRINK');
+INSERT INTO tb_category(name) values ('VEGAN');
+INSERT INTO tb_category(name) values ('ICE CREAM');
+
+
