6145523ae5644bc6505a21fa0a9450a047d63186
bruno pinho <iaishowcs@gmail.com>
2021-06-15 20:03:21 -0300

    Implement Seed Product SQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,19 @@
 INSERT INTO tb_category(name) values ('PASTA');
 INSERT INTO tb_category(name) values ('PIZZA');
 INSERT INTO tb_category(name) values ('HAMBURGUER');
-INSERT INTO tb_category(name) values ('DESSERT');
 INSERT INTO tb_category(name) values ('DRINK');
 INSERT INTO tb_category(name) values ('VEGAN');
 INSERT INTO tb_category(name) values ('ICE CREAM');
+INSERT INTO tb_product(name,description,price) values ('Spaghetti Bolognese','A delicious pasta made with lots of love',20.00);
+INSERT INTO tb_product(name,description,price) values ('Spaghetti with garlic and oil','A perfect Spaghetti for your meal',25.00);
+INSERT INTO tb_product(name,description,price) values ('Pepperoni Pizza','A delicious pepperoni pizza made with the best ingredients',15.00);
+INSERT INTO tb_product(name,description,price) values ('Mozzarella Pizza','A delicious mozzarella pizza made with the best ingredients',15.00);
+INSERT INTO tb_product(name,description,price) values ('X Burguer','A perfect sandwich for your snack',10.00);
+INSERT INTO tb_product(name,description,price) values ('American Burger','A delicious sandwich with lots of spice and love',10.00);
+INSERT INTO tb_product(name,description,price) values ('Coke','A very cold and delicious coke',2.00);
+INSERT INTO tb_product(name,description,price) values ('Beer','A delicious beer',3.00);
+INSERT INTO tb_product(name,description,price) values ('Redbull','A drink to keep you awake',4.00);
+INSERT INTO tb_product(name,description,price) values ('Oranje Juice','A very tasty and natural juice',1.50);
+
 
 
